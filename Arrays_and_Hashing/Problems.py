a2e984a366de2d07998c77170a9b563cff259f6a
Abdul Ghani Butt <agbutt@widener.edu>
2025-08-28 08:52:16 -0400

    Second Problem: Is Anagram

--- Arrays_and_Hashing/Problems.py
+++ Arrays_and_Hashing/Problems.py
@@ -1,7 +1,7 @@
 # This file has all the problems in the Arrays & Hashing
 # section of the NeetCode150 with the explanations.
 
-# Problem-1:
+# Problem-1 (Contains Duplicate):
 def hasDuplicate(nums):
     duplicates = {}
 
@@ -12,3 +12,17 @@ def hasDuplicate(nums):
         duplicates[n] = duplicates.get(n, 0) + 1
 
     return False
+
+# Problem-2 (Is Anagram):
+def isAnagram(s, t):
+    freq_1 = {}
+    freq_2 = {}
+
+    for c in s:
+        freq_1[c] = freq_1.get(c, 0) + 1
+
+    for c in t:
+        freq_2[c] = freq_2.get(c, 0) + 1
+
+    return freq_1 == freq_2
+
