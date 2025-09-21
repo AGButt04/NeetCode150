ce10d64607207033fbc6e4a681218e2a09ad1df0
Abdul Ghani Butt <agbutt@widener.edu>
2025-09-20 21:41:35 -0400

    Add Two Numbers

--- LinkedList/Problems.py
+++ LinkedList/Problems.py
@@ -125,8 +125,30 @@ def removeNthFromEnd(head, n):
         walker = walker.next
         count += 1
 
-    return head
-
     # Time complexity: O(n) for iterating twice the list O(n + n) => O(n)
     # Space complexity: O(1) for just storing handful of variables.
     return head
+
+def addTwoNumbers(l1, l2):
+    newHead = ListNode()
+    curr = newHead
+    carry = 0
+
+    while l1 or l2 or carry:
+        v1 = l1.val if l1 else 0
+        v2 = l2.val if l2 else 0
+        val = v1 + v2 + carry
+
+        carry = val // 10
+        val = val % 10
+
+        node = ListNode(val)
+        curr.next = node
+
+        l1 = l1.next if l1 else None
+        l2 = l2.next if l2 else None
+        curr = curr.next
+
+    return newHead.next
+
+
