9942fc469e3095ac0e475b3f2fea1862957f7386
Abdul Ghani Butt <agbutt@widener.edu>
2025-10-21 15:35:44 -0400

    clone Graph Node class

--- Graphs/Problems.py
+++ Graphs/Problems.py
@@ -3,7 +3,12 @@
 
 from collections import deque
 
-def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+
+def cloneGraph(self, node):
     if not node:
         return None
 
