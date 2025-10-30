b8690e8bc2d1543a461517aebf46de06339ca61e
Abdul Ghani Butt <agbutt@widener.edu>
2025-10-30 13:31:29 -0400

    longest Palindromic String using Two pointers

--- 1-D_DynammicPrograming/Problems.py
+++ 1-D_DynammicPrograming/Problems.py
@@ -87,3 +87,35 @@ def robbing(self, nums):
 
     return dp[-1]
 
+def longestPalindrome(self, s):
+    n = len(s)
+    if n == 1:
+        return s
+
+    start = 0
+    length = 0
+
+    for i in range(n):
+        # odd length
+        left = i
+        right = i
+
+        while left >= 0 and right < n and s[left] == s[right]:
+            if (right - left + 1) > length:
+                start = left
+                length = right - left + 1
+            left -= 1
+            right += 1
+
+        # even length
+        left = i
+        right = i + 1
+
+        while left >= 0 and right < n and s[left] == s[right]:
+            if (right - left + 1) > length:
+                start = left
+                length = right - left + 1
+            left -= 1
+            right += 1
+
+    return s[start: start + length]
