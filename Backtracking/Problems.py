48721dbed8ac843d7c645e11f4d79d586d737c71
Abdul Ghani Butt <agbutt@widener.edu>
2025-11-12 16:33:06 -0500

    Generate Parenthesis

--- Backtracking/Problems.py
+++ Backtracking/Problems.py
@@ -17,3 +17,20 @@ def subsets(nums):
 
     recurse(0)
     return subsets
+
+def generateParenthesis(self, n: int) -> List[str]:
+    parenthesis = []
+
+    def recurse(string, open_count, close_count):
+        if open_count < close_count or open_count > n:
+            return
+        if len(string) == n * 2:
+            parenthesis.append(string)
+            return
+
+        recurse(string + "(", open_count + 1, close_count)
+        recurse(string + ")", open_count, close_count + 1)
+
+    recurse("", 0, 0)
+
+    return parenthesis
