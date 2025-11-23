415a22aca3cd6747030db68238534cd553216fe7
Abdul Ghani Butt <agbutt@widener.edu>
2025-11-22 20:04:37 -0500

    Set Zeroes

--- Math_and_Geometry/Problems.py
+++ Math_and_Geometry/Problems.py
@@ -1,7 +1,7 @@
 # This file has all the problems in the Backtracking
 # section of the NeetCode150 with the explanations.
 
-def rotate(self, matrix: List[List[int]]) -> None:
+def rotate(self, matrix) -> None:
     m = len(matrix)
     n = len(matrix[0])
     matrix.reverse()
@@ -11,3 +11,28 @@ def rotate(self, matrix: List[List[int]]) -> None:
             temp = matrix[i][j]
             matrix[i][j] = matrix[j][i]
             matrix[j][i] = temp
+
+def setZeroes(self, matrix) -> None:
+    m = len(matrix)
+    n = len(matrix[0])
+    stack = []
+
+    for i in range(m):
+        for j in range(n):
+            if matrix[i][j] == 0:
+                stack.append((i, j))
+
+    while stack:
+        row, col = stack.pop()
+        # Up
+        for i in range(row - 1, -1, -1):
+            matrix[i][col] = 0
+            # Down
+        for i in range(row + 1, m, 1):
+            matrix[i][col] = 0
+        # Left
+        for i in range(col - 1, -1, -1):
+            matrix[row][i] = 0
+        # Right
+        for i in range(col + 1, n, 1):
+            matrix[row][i] = 0
