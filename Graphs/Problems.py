e7311b3aa74029f23198867ed75af5f3d7a3b92d
Abdul Ghani Butt <agbutt@widener.edu>
2025-11-24 20:53:09 -0500

    solve

--- Graphs/Problems.py
+++ Graphs/Problems.py
@@ -325,8 +325,8 @@ def solve(self, board):
 
     for i in range(m):
         for j in range(n):
-            if (i == 0 or i == m - 1 or j == 0 or j == n - 1):
-                if (board[i][j] == 'O'):
+            if i == 0 or i == m - 1 or j == 0 or j == n - 1:
+                if board[i][j] == 'O':
                     self.BFS(i, j, board)
 
     for i in range(m):
