858e15435da1fb73e878a8b4631cd4a6439d52b9
Abdul Ghani Butt <agbutt@widener.edu>
2025-11-26 21:23:09 -0500

    Can Complete Circuit

--- Greedy/Problems.py
+++ Greedy/Problems.py
@@ -50,3 +50,19 @@ def jump(self, nums)  -> int:
         jumps += 1
 
     return jumps
+
+def canCompleteCircuit(self, gas, cost):
+    if sum(gas) < sum(cost):
+        return -1
+
+    total = 0
+    index = 0
+
+    for i in range(len(gas)):
+        total += gas[i] - cost[i]
+
+        if total < 0:
+            total = 0
+            index = i + 1
+
+    return index
