bc9fbc627acce2af226e96a24b70a0a2ff0d0ee4
Abdul Ghani Butt <agbutt@widener.edu>
2025-12-13 20:40:08 -0500

    find Target Sum Ways (Memoized)

--- 2-D_DynammicPrograming/Problems.py
+++ 2-D_DynammicPrograming/Problems.py
@@ -12,3 +12,22 @@ def recurse(self, nums, target, curr, idx):
     add = self.recurse(nums, target, curr + nums[idx], idx + 1)
 
     return sub + add
+
+# Memoized:
+def findTargetSumWays(self, nums, target: int) -> int:
+    memo = {}
+    ways = self.recurse(memo, nums, target, 0, 0)
+    return ways
+
+def recurse(self, memo, nums, target, curr, idx):
+    if idx == len(nums):
+        return 1 if target == curr else 0
+    if (idx, curr) in memo:
+        return memo[(idx, curr)]
+
+    sub = self.recurse(memo, nums, target, curr - nums[idx], idx + 1)
+    add = self.recurse(memo, nums, target, curr + nums[idx], idx + 1)
+    ways = sub + add
+    memo[(idx, curr)] = ways
+
+    return memo[(idx, curr)]
