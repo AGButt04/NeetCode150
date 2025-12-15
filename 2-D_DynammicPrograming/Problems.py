323ae4f72e8ea3c3e9a2d4ac96763785fff27cdb
Abdul Ghani Butt <agbutt@widener.edu>
2025-12-15 12:10:44 -0500

    longest Common Subsequence (Tabulation)

--- 2-D_DynammicPrograming/Problems.py
+++ 2-D_DynammicPrograming/Problems.py
@@ -133,3 +133,16 @@ def recurse(self, i, j, t1, t2, memo):
     next = self.recurse(i + 1, j, t1, t2, memo)
     memo[(i, j)] = max(curr, next)
     return memo[(i, j)]
+
+def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+    dp = [[0 for j in range(len(text2) + 1)]
+          for i in range(len(text1) + 1)]
+
+    for i in range(len(text1) - 1, -1, -1):
+        for j in range(len(text2) - 1, -1, -1):
+            if text1[i] == text2[j]:
+                dp[i][j] = 1 + dp[i + 1][j + 1]
+            else:
+                dp[i][j] = max(dp[i][j + 1], dp[i + 1][j])
+
+    return dp[0][0]
